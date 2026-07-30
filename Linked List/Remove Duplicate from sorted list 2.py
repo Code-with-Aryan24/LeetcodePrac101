e81cd7db903e7e0109ac09a012d122f23b644dc1
Code-with-Aryan24 <aryan77p7@gmail.com>
2026-07-30 15:40:24 +0530

    Following pointers logic

---
+++ Linked List/Remove Duplicate from sorted list 2.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Create a dummy 
+        dummy = ListNode(0, head)
+        prev = dummy
+        
+        while head:
+            # Skip nodes with duplicate values
+            while head.next and head.val == head.next.val:
+                head = head.next
+                
+            
+            if prev.next == head:
+                prev = prev.next
+            else:
+              
+                prev.next = head.next
+                
+           
+            head = head.next
+            
+        return dummy.next
